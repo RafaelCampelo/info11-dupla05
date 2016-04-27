d3706d1f7baacf9d34b4f05a7e0ce0ec0da4f718
Rafael Souto Campelo <rafael_campelo_91@hotmail.com>
2016-04-27 18:33:36 -0300

    iniciando NIM

--- ex11.c
+++ ex11.c
@@ -4,7 +4,39 @@
 
 int main(void)
 {
-    int cen,dez,uni,saida,entrada;
+    int valor, x, y, z;
 
-    scanf("%d", &entrada);
+     printf("Inicio do jogo! \n");
+     printf("Tabuleiro inicial: 3 5 7 \n");
+     printf("Digite se quer jogar no 3, no 5 ou no 7. \n");
+     scanf("%d", &a);
+     
+     x=3;
+     y=5;
+     z=7;
     
+     switch(a)
+     {
+         case 3:
+         printf("Digite quantos quer tirar: ");
+         scanf("%d", &valor);
+     
+         switch(valor)
+         {
+             case 1:
+             printf("O tabuleiro atualizado eh: %d %d %d\n", x-1, y, z);
+             break;
+     
+             case 2:
+             printf("O tabuleiro atualizado eh: %d %d %d\n", x-2, y, z);
+             break;
+             
+             case 3:
+             printf("O tabuleiro atualizado eh: %d %d %d\n", x-3, y, z);
+             break;
+
+             default:
+             printf("Opcao invalida!\n");
+         }
+         break;
+
